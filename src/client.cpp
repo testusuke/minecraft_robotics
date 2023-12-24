47b65d63871119c6881ef59e3b1f7ada7dde2796
testusuke <testusuke@gmail.com>
2023-12-25 00:25:52 +0900

    feat: add sample code

--- src/client.cpp
+++ src/client.cpp
@@ -1,41 +1,90 @@
 #include <chrono>
 #include <functional>
 #include <memory>
-#include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+
+#include "LinuxHardwareSerial.hpp"
+#include "SerialBridge.hpp"
+#include "Vector3.hpp"
+
+#define SERIAL_PORT "/dev/serial/by-path/pci-0000:00:14.0-usb-0:6:1.2"
 
 using namespace std::chrono_literals;
+using std::placeholders::_1;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
 
-class SerialBridgeClient : public rclcpp::Node
-{
+class SerialBridgeClient : public rclcpp::Node {
 public:
-    SerialBridgeClient()
-            : Node("serial_bridge_client")
-    {
+    SerialBridgeClient(const char port[], speed_t baud_rate = B9600)
+            : Node("serial_bridge_client"), serial_dev(new LinuxHardwareSerial(port, baud_rate)),
+              serial(new SerialBridge(serial_dev, 1024)) {
+        //  add frame
+        serial->add_frame(0, &vector_msg[0]);
+        serial->add_frame(1, &vector_msg[1]);
+
+        //  setup subscription
+        subscription_ = this->create_subscription<geometry_msgs::msg::Vector3>(
+                "serial_bridge_example_to_serial", 10, std::bind(&SerialBridgeClient::topic_callback, this, _1));
+
+        //  setup publisher
+        publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("serial_bridge_example_from_serial", 10);
+
+        //  serial bridge watchdog
+        timer_ = this->create_wall_timer(
+                100ms, std::bind(&SerialBridgeClient::serial_callback, this));
     }
 
 private:
-    void timer_callback()
-    {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        publisher_->publish(message);
+
+    void serial_callback() {
+        serial->update();
+
+        if (vector_msg[1].was_updated()) {
+            geometry_msgs::msg::Vector3 vector3;
+
+            //  copy
+            vector3.set__x(vector_msg[1].data.x);
+            vector3.set__y(vector_msg[1].data.y);
+            vector3.set__z(vector_msg[1].data.z);
+
+            publisher_->publish(vector3);
+
+            RCLCPP_DEBUG(this->get_logger(), "Received message from serial");
+        }
     }
+
+    void topic_callback(const geometry_msgs::msg::Vector3 &msg) {
+        //  replace
+        vector_msg[0].data.x = (float) msg.x;
+        vector_msg[0].data.y = (float) msg.y;
+        vector_msg[0].data.z = (float) msg.z;
+
+        //  send to serial
+        serial->write(0);
+
+        RCLCPP_DEBUG(this->get_logger(), "Send message to serial");
+    }
+
+    //  publisher
+    rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr publisher_;
+    //  subscription
+    rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr subscription_;
+
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+
+    //  Serial Bridge
+    SerialDev *serial_dev;
+    SerialBridge *serial;
+
+    //  Message
+    Vector3 vector_msg[2];
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<SerialBridgeClient>());
+    rclcpp::spin(std::make_shared<SerialBridgeClient>(SERIAL_PORT));
     rclcpp::shutdown();
     return 0;
 }
